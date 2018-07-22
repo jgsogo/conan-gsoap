c0b08582d55971fc5201cf06ac5f41beccd09398
Javier G. Sogo <jgsogo@gmail.com>
2018-07-22 17:42:30 +0200

    use cmake for linux

--- conanfile.py
+++ conanfile.py
@@ -45,38 +45,19 @@ class GSoap(ConanFile):
             tools.get("https://sourceforge.net/projects/gsoap2/files/oldreleases/{name}_{version}.zip/download".format(name=self.name, version=self.version))
 
     def build(self):
-        if self.settings.os == "Windows":
-            cmake = CMake(self)
-            cmake.definitions["GSOAP_PATH"] = os.path.join(self.source_folder, self.lib_name).replace('\\', '/')
-            if self.options.with_openssl:
-                cmake.definitions["WITH_OPENSSL"] = True
-                cmake.definitions["WITH_GZIP"] = True
-            if self.options.with_ipv6:
-                cmake.definitions["WITH_IPV6"] = True
-            cmake.definitions["WITH_COOKIES"] = self.options.with_cookies
-            cmake.definitions["WITH_C_LOCALE"] = self.options.with_c_locale
+        cmake = CMake(self)
+        cmake.definitions["GSOAP_PATH"] = os.path.join(self.source_folder, self.lib_name).replace('\\', '/')
+        if self.options.with_openssl:
+            cmake.definitions["WITH_OPENSSL"] = True
+            cmake.definitions["WITH_GZIP"] = True
+        if self.options.with_ipv6:
+            cmake.definitions["WITH_IPV6"] = True
+        cmake.definitions["WITH_COOKIES"] = self.options.with_cookies
+        cmake.definitions["WITH_C_LOCALE"] = self.options.with_c_locale
 
-            cmake.configure(source_folder="src")
-            cmake.build()
-            cmake.install()
-
-        else:
-            with chdir(self.lib_name):
-                self.run('chmod +x configure')
-                self.run('./configure --help')
-                env_build = AutoToolsBuildEnvironment(self)
-                self.run('autoreconf -f -i')  # Fix out of date aclocal
-                env_build.configure(args=['--prefix', self.package_folder,
-                                          '--with-openssl={}'.format(self.deps_cpp_info["OpenSSL"].rootpath),
-                                          '--with-zlib={}'.format(self.deps_cpp_info["zlib"].rootpath),
-                                          '--enable-ipv6' if self.options.with_ipv6 else '--disable-ipv6',
-                                          #'--enable-cookies' if self.options.with_cookies else '--disable-cookies',
-                                          '--enable-c-locale' if self.options.with_c_locale else '--disable-c-locale',
-                                          '--enable-debug' if self.settings.build_type == 'Debug' else '',
-                                          ],
-                                    build=False)
-                env_build.make(args=["-j1", ],)  # Weird, but with -j2 it fails
-                env_build.make(args=['install', ])
+        cmake.configure(source_folder="src")
+        cmake.build()
+        cmake.install()
 
     def package(self):
         self.copy("FindGSOAP.cmake", dst=".", src=".")
